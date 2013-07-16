218b9f98187bca518f1bb31f3d5eae3adf2e5495
Pankaj Saharan <pankaj.saharan@gmail.com>
2013-07-16 23:14:34 +0000

    Changes: 1. Changed grader.js to user restler and cheerio for url parsing 2. Enabled 8080 port for public access for local development in web.js 3. Updated styling and responsiveness for index.html

--- grader.js
+++ grader.js
@@ -22,10 +22,14 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://google.com";
+var urlContents = " ";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,6 +40,18 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertURLExists = function(urlPath) {
+	console.log("HEREEEEEEEE in asserting that url exists" + urlPath);
+	urlContents = rest.get(urlPath).on('complete', function(result) {
+		if (result instanceof Error) {
+			sys.puts('Error: ' + result.message);
+			process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+		}
+	});
+	console.log(urlContents);
+	return true;
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -43,6 +59,11 @@ var cheerioHtmlFile = function(htmlfile) {
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
+
+var checkURLPath = function(urlPath, checksfile) {
+    checkHtmlFile(urlContents, checksfile);
+};
+
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
@@ -65,8 +86,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'URL path', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	if (program.file == HTMLFILE_DEFAULT && program.url != URL_DEFAULT) {
+		console.log("HEREEEEEEEEEEEEEEEEE HTML file is default &&& URL is PRESENT!!" + program.url);
+		var checkJson = checkURLPath(program.url, program.checks);
+	} else {
+		console.log(" URL is ABSENT!!");
+		var checkJson = checkHtmlFile(program.file, program.checks);
+	}
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
