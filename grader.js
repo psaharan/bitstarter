eb71f167a1a1a57943c2245f58ff488917614d0a
Pankaj Saharan <pankaj.saharan@gmail.com>
2013-07-16 23:34:14 +0000

    Revert "Changes:"
    This reverts commit 218b9f98187bca518f1bb31f3d5eae3adf2e5495.

--- grader.js
+++ grader.js
@@ -22,14 +22,10 @@ References:
 */
 
 var fs = require('fs');
-var sys = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://google.com";
-var urlContents = " ";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -40,18 +36,6 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var assertURLExists = function(urlPath) {
-	console.log("HEREEEEEEEE in asserting that url exists" + urlPath);
-	urlContents = rest.get(urlPath).on('complete', function(result) {
-		if (result instanceof Error) {
-			sys.puts('Error: ' + result.message);
-			process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-		}
-	});
-	console.log(urlContents);
-	return true;
-};
-
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -59,11 +43,6 @@ var cheerioHtmlFile = function(htmlfile) {
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
-
-var checkURLPath = function(urlPath, checksfile) {
-    checkHtmlFile(urlContents, checksfile);
-};
-
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
@@ -86,15 +65,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-		.option('-u, --url <url>', 'URL path', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
-	if (program.file == HTMLFILE_DEFAULT && program.url != URL_DEFAULT) {
-		console.log("HEREEEEEEEEEEEEEEEEE HTML file is default &&& URL is PRESENT!!" + program.url);
-		var checkJson = checkURLPath(program.url, program.checks);
-	} else {
-		console.log(" URL is ABSENT!!");
-		var checkJson = checkHtmlFile(program.file, program.checks);
-	}
+    var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
